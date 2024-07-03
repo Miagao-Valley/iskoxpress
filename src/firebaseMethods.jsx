50cdddbc046e342042ca00e79516c27692094e9b
lueezzzz <rcmabait@up.edu.ph>
2024-07-03 14:50:23 +0800

    login with up mail

--- src/firebaseMethods.jsx
+++ src/firebaseMethods.jsx
@@ -1,12 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithRedirect, GoogleAuthProvider } from "firebase/auth";
 
-
-
 const firebase = {
-  apiKey: "import.meta.env.VITE_API_KEY",
+  apiKey: import.meta.env.VITE_API_KEY,
   authDomain: "iskoxpress.firebaseapp.com",
   projectId: "iskoxpress",
+  storageBucket: "iskoxpress.appspot.com",
+  messagingSenderId: "547003784449",
+  appId: "1:547003784449:web:432a75c6a935409541cf00",
+  measurementId: "G-XC2M0ZZ7W2",
 };
 
 export const app = initializeApp(firebase);
@@ -14,6 +16,7 @@ export const auth = getAuth();
 export const provider = new GoogleAuthProvider();
 
 export const googleLogin = () => {
+  
   // makes sure the users signs in with a up mail.
   provider.setCustomParameters({
     hd: "up.edu.ph",
