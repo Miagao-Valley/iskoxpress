175d5d80c0a90f65aa04cbc48821a6f3d7748a66
lueezzzz <rianmabait45@gmail.com>
2025-06-10 02:18:35 +0800

    [Rian][Frontend] removable image preview

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,3 @@
-import ExpressForm from "@/components/express/express-form";
-import { Box } from "@chakra-ui/react";
-
 export default function HomePage() {
-    return (
-        <>
-            <Box>
-                <ExpressForm />
-            </Box>
-        </>
-    );
+    return <></>;
 }
